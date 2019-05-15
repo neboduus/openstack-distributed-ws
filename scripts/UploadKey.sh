bbf90ec3a6e092dcb85fddf9849afdf441aaff68
armaniv <valentino.armani@studenti.unitn.it>
2019-05-15 22:06:37 +0200

    minor change UploadKey.sh

--- scripts/UploadKey.sh
+++ scripts/UploadKey.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
-for entry in `ls $search_path | grep *.pub`; do
-	scp $entry ubuntu@217.77.95.88:keyteam
-done
+# put the .pub files in this direcotry in ubuntu@217.77.95.88:.ssh/
+# for entry in `ls $search_path | grep *.pub`; do
+	# scp $entry ubuntu@217.77.95.88:.ssh/
+# done
 
-KEYDOC1="valentino.key.pub"
-KEYDOC2="marian_key.pub"
+# edit the name of the key
+KEYDOC1="daniele.pizzolli.key.pub"
 
+# upload key on the VMs
 ssh ubuntu@217.77.95.88 -A << EOF
-	ssh-copy-id -f -i keyteam/$KEYDOC1 ubuntu@172.24.4.25
-	ssh-copy-id -f -i keyteam/$KEYDOC1 ubuntu@172.24.4.6
-	ssh-copy-id -f -i keyteam/$KEYDOC1 ubuntu@172.24.4.21
-	ssh-copy-id -f -i keyteam/$KEYDOC1 ubuntu@172.24.4.28
-	ssh-copy-id -f -i keyteam/$KEYDOC2 ubuntu@172.24.4.25
-	ssh-copy-id -f -i keyteam/$KEYDOC2 ubuntu@172.24.4.6
-	ssh-copy-id -f -i keyteam/$KEYDOC2 ubuntu@172.24.4.21
-	ssh-copy-id -f -i keyteam/$KEYDOC2 ubuntu@172.24.4.28
+	ssh-copy-id -f -i .ssh/$KEYDOC1 ubuntu@172.24.4.25
+	ssh-copy-id -f -i .ssh/$KEYDOC1 ubuntu@172.24.4.6
+	ssh-copy-id -f -i .ssh/$KEYDOC1 ubuntu@172.24.4.21
+	ssh-copy-id -f -i .ssh/$KEYDOC1 ubuntu@172.24.4.28
 EOF
