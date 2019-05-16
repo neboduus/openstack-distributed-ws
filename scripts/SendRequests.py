c030589d07c1e3bcd2b2ddcd1e9301cd66c068da
Marian Diaconu <neboduus@gmail.com>
2019-05-16 14:49:14 +0200

    adds progress bar

--- scripts/SendRequests.py
+++ scripts/SendRequests.py
@@ -3,11 +3,10 @@
 
 import json
 import requests
+import progressbar
 from collections import Counter
 
-
-print("ToDo\n")
-
+print("Serving requests to Todos-WS...")
 
 URL = "http://172.24.4.21/todos/1"
 
@@ -15,16 +14,23 @@ res_ip = []
 
 tot_num_req = 40
 
+bar = progressbar.ProgressBar(maxval=tot_num_req, \
+    widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+bar.start()
+
 for i in range(0, tot_num_req):
-	r = requests.get(url = URL)
-	data = r.json()
-	res_ip.append(data['ip'])
+    bar.update(i+1)
+    r = requests.get(url = URL)
+    data = r.json()
+    res_ip.append(data['ip'])
+
+bar.finish()
 
 different_ip = Counter(res_ip).keys()
 counter_x_ip = Counter(res_ip).values()
 
 for i in range (0, len(different_ip)):
-	ip = different_ip[i]
-	count = counter_x_ip[i]
-	percent = (count / float(tot_num_req)) * 100
-	print("%s serves %s requests (%s%%)" % (ip, count, round(percent, 1)))
+    ip = different_ip[i]
+    count = counter_x_ip[i]
+    percent = (count / float(tot_num_req)) * 100
+    print("%s serves %s requests (%s%%)" % (ip, count, round(percent, 1)))
